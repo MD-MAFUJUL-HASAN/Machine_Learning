1b8c90ec21fe2e09992bf99513ac54c7d423aa5b
MD MAFUJUL HASAN <128472454+MD-MAFUJUL-HASAN@users.noreply.github.com>
2023-03-26 15:20:55 +0600

    Delete Web directory

--- Web/Animated-Login-Form-2-master/README.md
+++
@@ -1,3 +0,0 @@
-# Animated-Login-Form-2
-follow me on all social medai @techvaness
-follow me on instagram to get updates on my #100DaysOfCode journey
--- Web/Navigation-Tabs-Menu---2023-master/script.js
+++
@@ -1,8 +0,0 @@
-let list = document.querySelectorAll('.navigation li');
-function activeLink(){
-    list.forEach((item) =>
-    item.classList.remove('active'));
-    this.classList.add('active');
-}
-list.forEach((item) =>
-item.addEventListener('click', activeLink))
